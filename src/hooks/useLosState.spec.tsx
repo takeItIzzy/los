96302e3343949db02daa0e4aabefa67adb9527aa
takeItIzzy <q1367257497@gmail.com>
2022-05-02 04:09:04 +0800

    test(useLosState): finished unit testing of useLosState

--- src/hooks/useLosState.spec.tsx
+++ src/hooks/useLosState.spec.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
-import { render, renderHook, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
-import { atom } from '../store';
-import { useLosState } from './useLosState';
-
-const atomState = atom({ defaultValue: 1 });
-const Wrapper = () => {
-  const [state, setState] = useLosState(atomState);
-
-  return (
-    <div>
-      <span data-testid="state">{state}</span>
-      <button data-testid="increment" onClick={() => setState(2)}>
-        Increment
-      </button>
-    </div>
-  );
-};
+import { Atom, atom } from '../store';
+import { useLosState, useLosValue } from './useLosState';
 
 describe('useLosState() testing', () => {
-  it("should useLosState return state's defaultValue", () => {
-    const state = atom({ defaultValue: 1 });
-    const { result } = renderHook(() => useLosState(state));
+  let atomState: Atom<number>, Wrapper: React.ElementType;
+  beforeEach(() => {
+    atomState = atom({ defaultValue: 1 });
+    Wrapper = () => {
+      const [state, setState] = useLosState<number>(atomState);
 
-    expect(result.current[0]).toBe(1);
+      return (
+        <div>
+          <span data-testid="state">{state}</span>
+          <button data-testid="increment" onClick={() => setState(2)}>
+            Increment
+          </button>
+        </div>
+      );
+    };
+  });
+
+  it("should useLosState return state's defaultValue", () => {
+    render(<Wrapper />);
+
+    expect(screen.getByTestId('state')).toHaveTextContent('1');
   });
 
   it('should useLosState return the new state after setState', async () => {
@@ -34,5 +36,51 @@ describe('useLosState() testing', () => {
     await user.click(screen.getByTestId('increment'));
 
     expect(screen.getByTestId('state')).toHaveTextContent('2');
+  });
+
+  it('the argument of setState is a callback function', async () => {
+    const atomState = atom({ defaultValue: 1 });
+    const Wrapper = () => {
+      const [state, setState] = useLosState(atomState);
+
+      return (
+        <div>
+          <span data-testid="state">{state}</span>
+          <button data-testid="increment" onClick={() => setState((prev) => prev + 3)}>
+            Increment
+          </button>
+        </div>
+      );
+    };
+
+    const user = userEvent.setup();
+    render(<Wrapper />);
+
+    await user.click(screen.getByTestId('increment'));
+
+    expect(screen.getByTestId('state')).toHaveTextContent('4');
+  });
+
+  it('should any components that use the atom are updated to the latest value', async () => {
+    const Wrapper2 = () => {
+      const state = useLosValue(atomState);
+
+      return <div data-testid="wrapper2">another component: {state}</div>;
+    };
+    const Parent = () => (
+      <>
+        <Wrapper />
+        <Wrapper2 />
+      </>
+    );
+
+    render(<Parent />);
+
+    expect(screen.getByTestId('wrapper2')).toHaveTextContent('another component: 1');
+
+    const user = userEvent.setup();
+    await user.click(screen.getByTestId('increment'));
+
+    expect(screen.getByTestId('wrapper2')).toHaveTextContent('another component: 2');
   });
 });
